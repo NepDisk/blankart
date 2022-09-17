8979dafd4cefeb2540863e53a3b95a37f53f11b9
James R <justsomejames2@gmail.com>
2022-09-17 06:34:11 -0700

    Replace shitty item box pop with flying debris and dust clouds
    Debris flies forward and outward from the player in the
    direction of momentum. Debris particles bounce once then
    disappear when they hit the ground for the second time.
    Clouds spawn on and trail behind the player for a short
    duration.

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -1209,6 +1209,9 @@ fixed_t P_GetMobjGravity(mobj_t *mo)
 				case MT_KARMAFIREWORK:
 					gravityadd /= 3;
 					break;
+				case MT_ITEM_DEBRIS:
+					gravityadd *= 6;
+					break;
 				default:
 					break;
 			}
@@ -2339,6 +2342,15 @@ boolean P_ZMovement(mobj_t *mo)
 			mom.z = P_MobjFlip(mo)*FixedMul(5*FRACUNIT, mo->scale);
 		else if (mo->type == MT_SPINFIRE) // elemental shield fire is another exception here
 			;
+		else if (mo->type == MT_ITEM_DEBRIS)
+		{
+			mom.z = Obj_ItemDebrisBounce(mo, mom.z);
+
+			if (mom.z == 0)
+			{
+				return false;
+			}
+		}
 		else if (mo->type == MT_DRIFTCLIP)
 		{
 			mom.z = -mom.z/2;
@@ -7884,6 +7896,11 @@ static boolean P_MobjRegularThink(mobj_t *mobj)
 	case MT_HYUDORO_CENTER:
 	{
 		Obj_HyudoroCenterThink(mobj);
+		break;
+	}
+	case MT_ITEM_DEBRIS:
+	{
+		Obj_ItemDebrisThink(mobj);
 		break;
 	}
 	case MT_ROCKETSNEAKER:
