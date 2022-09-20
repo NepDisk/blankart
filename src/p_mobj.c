b0c7a9484cd14a6e4321e5b66c88f921f1538a02
James R <justsomejames2@gmail.com>
2022-09-19 23:37:29 -0700

    Reenable item box respawning
    Old code was shit so I removed it (4d67cc632). Turns out
    none of that bullshit actually mattered to make this box
    respawn and it just needs to go to an invisible state!
    
    Makes use of some nifty flickering code (that was already
    there but effectively disabled) shortly before it actually
    respawns.

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -12692,10 +12692,6 @@ static boolean P_SetupSpawnedMapThing(mapthing_t *mthing, mobj_t *mobj, boolean 
 				P_SetThingPosition(mobj);
 			}
 		}
-		else
-		{
-			P_SpawnMobj(mobj->x, mobj->y, mobj->z, MT_EXPLODE);
-		}
 		break;
 	}
 	case MT_ITEMCAPSULE:
