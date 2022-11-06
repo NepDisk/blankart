607c5c5dae0de3cc9a3cd9f875c149b513b0072b
Sally Coolatta <tehrealsalt@gmail.com>
2022-11-06 14:58:31 -0500

    SubtractNum also makes tripwire subtractive

--- src/p_setup.c
+++ src/p_setup.c
@@ -3090,6 +3090,7 @@ static void P_ProcessLinedefsAfterSidedefs(void)
 {
 	size_t i = numlines;
 	register line_t *ld = lines;
+	const boolean subtractTripwire = ((mapheaderinfo[gamemap - 1]->levelflags & LF_SUBTRACTNUM) == LF_SUBTRACTNUM);
 
 	for (; i--; ld++)
 	{
@@ -3104,7 +3105,7 @@ static void P_ProcessLinedefsAfterSidedefs(void)
 
 		if (ld->tripwire)
 		{
-			ld->blendmode = AST_ADD;
+			ld->blendmode = (subtractTripwire ? AST_SUBTRACT : AST_ADD);
 			ld->alpha = FRACUNIT;
 		}
 
