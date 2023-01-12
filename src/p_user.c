ca03f2b7779b5fdf9ad3fc18353190cc22aca476
Oni <velocitoni@gmail.com>
2023-01-12 00:35:08 +0000

    Merge branch 'bot-realtime-fix' into 'master'
    Bot `realtime` fix
    
    See merge request KartKrew/Kart!874

--- src/p_user.c
+++ src/p_user.c
@@ -2661,7 +2661,7 @@ static void P_DeathThink(player_t *player)
 	}
 
 	// Keep time rolling
-	if (!(exitcountdown && !racecountdown) && !(player->exiting || mapreset) && !(player->pflags & PF_NOCONTEST) && !stoppedclock)
+	if (!(player->exiting || mapreset) && !(player->pflags & PF_NOCONTEST) && !stoppedclock)
 	{
 		if (leveltime >= starttime)
 		{
@@ -4288,7 +4288,7 @@ void P_PlayerThink(player_t *player)
 	}
 
 	// Synchronizes the "real" amount of time spent in the level.
-	if (!(exitcountdown && !racecountdown) && !(player->exiting || mapreset) && !(player->pflags & PF_NOCONTEST) && !stoppedclock)
+	if (!(player->exiting || mapreset) && !(player->pflags & PF_NOCONTEST) && !stoppedclock)
 	{
 		if (leveltime >= starttime)
 		{
