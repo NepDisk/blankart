6b087c30781c6cc875ab84852c96e886f06d78f8
James R <justsomejames2@gmail.com>
2023-03-19 22:40:43 -0700

    Reset TERRAIN when respawning
    Fixes damage floor TERRAIN applying for the entire
    duration of lightsnake.

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -3726,6 +3726,7 @@ static void P_PlayerMobjThinker(mobj_t *mobj)
 		P_CheckPosition(mobj, mobj->x, mobj->y);
 		mobj->floorz = tmfloorz;
 		mobj->ceilingz = tmceilingz;
+		mobj->terrain = NULL;
 		goto animonly;
 	}
 
