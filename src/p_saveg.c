4d55a2ebe3abafef00902fbeb014e7b48daee878
toaster <rollerorbital@gmail.com>
2023-04-13 20:39:40 +0100

    P_NetUnArchiveThinkers: set thinker references to 0 before removing
    Fixes failed assertion on server join (resolves #514)

--- src/p_saveg.c
+++ src/p_saveg.c
@@ -4298,6 +4298,8 @@ static void P_NetUnArchiveThinkers(savebuffer_t *save)
 		{
 			next = currentthinker->next;
 
+			currentthinker->references = 0; // Heinous but this is the only place the assertion in P_UnlinkThinkers is wrong
+
 			if (currentthinker->function.acp1 == (actionf_p1)P_MobjThinker || currentthinker->function.acp1 == (actionf_p1)P_NullPrecipThinker)
 				P_RemoveSavegameMobj((mobj_t *)currentthinker); // item isn't saved, don't remove it
 			else
