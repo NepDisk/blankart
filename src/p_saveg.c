78f7cadd9b09021eb3cfa76b631da68aa5686505
Oni <velocitoni@gmail.com>
2023-05-15 02:58:31 +0000

    Merge branch 'cull-debugwaypoints' into 'master'
    debugwaypoints: cull debug mobjs outside of drawdist
    
    See merge request KartKrew/Kart!1201

--- src/p_saveg.c
+++ src/p_saveg.c
@@ -1665,6 +1665,10 @@ static void SaveMobjThinker(const thinker_t *th, const UINT8 type)
 	if (mobj->type == MT_HOOPCENTER && mobj->threshold == 4242)
 		return;
 
+	// MT_SPARK: used for debug stuff
+	if (mobj->type == MT_SPARK)
+		return;
+
 	if (mobj->spawnpoint)
 	{
 		// spawnpoint is not modified but we must save it since it is an identifier
@@ -4136,7 +4140,9 @@ static void P_RelinkPointers(void)
 
 		mobj = (mobj_t *)currentthinker;
 
-		if (mobj->type == MT_HOOP || mobj->type == MT_HOOPCOLLIDE || mobj->type == MT_HOOPCENTER)
+		if (mobj->type == MT_HOOP || mobj->type == MT_HOOPCOLLIDE || mobj->type == MT_HOOPCENTER
+			// MT_SPARK: used for debug stuff
+			|| mobj->type == MT_SPARK)
 			continue;
 
 		if (mobj->tracer)
@@ -4769,7 +4775,9 @@ void P_SaveNetGame(boolean resending)
 				continue;
 
 			mobj = (mobj_t *)th;
-			if (mobj->type == MT_HOOP || mobj->type == MT_HOOPCOLLIDE || mobj->type == MT_HOOPCENTER)
+			if (mobj->type == MT_HOOP || mobj->type == MT_HOOPCOLLIDE || mobj->type == MT_HOOPCENTER
+				// MT_SPARK: used for debug stuff
+				|| mobj->type == MT_SPARK)
 				continue;
 			mobj->mobjnum = i++;
 		}
