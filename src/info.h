c5214c000f72f7b5fd31842d6d937dfdc92bb96b
NepDisk <yoshikirby43@gmail.com>
2024-04-28 19:20:54 -0400

    Expose some constants to lua

--- src/info.h
+++ src/info.h
@@ -561,7 +561,7 @@ void A_SSChainShatter();
 extern boolean actionsoverridden[NUMACTIONS];
 
 // ratio of states to sprites to mobj types is roughly 6 : 1 : 1
-#define NUMMOBJFREESLOTS 1024
+#define NUMMOBJFREESLOTS 4096
 #define NUMSPRITEFREESLOTS NUMMOBJFREESLOTS
 #define NUMSTATEFREESLOTS (NUMMOBJFREESLOTS*8)
 
