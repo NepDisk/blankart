68482b421634e096ad3884010b91a5907fca3720
NepDisk <16447892+NepDisk@users.noreply.github.com>
2024-08-01 10:56:41 -0400

    Add evil mutex bypass hack in console
    https://git.do.srb2.org/KartKrew/RingRacers/-/commit/a68c863087bc21c7ec11aa6846db289961916ac7

--- src/sdl/i_system.c
+++ src/sdl/i_system.c
@@ -227,6 +227,7 @@ SDL_bool consolevent = SDL_FALSE;
 SDL_bool framebuffer = SDL_FALSE;
 
 UINT8 keyboard_started = false;
+boolean g_in_exiting_signal_handler = false;
 
 #ifdef UNIXBACKTRACE
 #define STDERR_WRITE(string) if (fd != -1) I_OutputMsg("%s", string)
@@ -360,6 +361,7 @@ static void I_ReportSignal(int num, int coredumped)
 #ifndef NEWSIGNALHANDLER
 FUNCNORETURN static ATTRNORETURN void signal_handler(INT32 num)
 {
+	g_in_exiting_signal_handler = true;
 	D_QuitNetGame(); // Fix server freezes
 	CL_AbortDownloadResume();
 #ifdef UNIXBACKTRACE
