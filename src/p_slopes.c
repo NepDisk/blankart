fd31992b72b679a85ea321c2d6eb72aad44ae1e9
NepDisk <16447892+NepDisk@users.noreply.github.com>
2024-08-07 14:05:36 -0400

    Reeanble flatslope check

--- src/p_slopes.c
+++ src/p_slopes.c
@@ -908,11 +908,11 @@ boolean P_CanApplySlopePhysics(mobj_t *mo, pslope_t *slope)
 		return false;
 	}
 
-	/*if (slope->normal.x == 0 && slope->normal.y == 0)
+	if (slope->normal.x == 0 && slope->normal.y == 0)
 	{
 		// Flat slope? No such thing, man. No such thing.
 		return false;
-	}*/
+	}
 
 	// We can do slope physics.
 	return true;
