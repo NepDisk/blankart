KartKrewDev/RingRacers

Update waypoints on same tic as crossing finish line
Fix 1 tic delay between crossing finish line and next
waypoint updating. May influence respawn behavior.

src/p_spec.c
@@ -1954,7 +1954,9 @@ static void K_HandleLapIncrement(player_t *player)
 				nump++;
 			}
 
+			player->cheatchecknum = 0;
 			player->laps++;
+			K_UpdateAllPlayerPositions();
 
 			if (G_TimeAttackStart() && !linecrossed)
 			{
@@ -1986,8 +1988,6 @@ static void K_HandleLapIncrement(player_t *player)
 
 			if (netgame && player->laps > numlaps)
 				CON_LogMessage(va(M_GetText("%s has finished the race.\n"), player_names[player-players]));
-
-			player->cheatchecknum = 0;
 
 			if (gametyperules & GTR_SPECIALSTART)
 			{
@@ -2182,6 +2182,7 @@ static void K_HandleLapDecrement(player_t *player)
 		{
 			player->cheatchecknum = numcheatchecks;
 			player->laps--;
+			K_UpdateAllPlayerPositions();
 			curlap = UINT32_MAX;
 		}
 	}

NepDisk/blankart

src/p_spec.c
@@ -1884,7 +1884,9 @@ static void K_HandleLapIncrement(player_t *player)
 				nump++;
 			}
 
+			player->starpostnum = 0;
 			player->laps++;
+			K_UpdateAllPlayerPositions();
 
 			// Set up lap animation vars
 			if (player->laps > 1)
@@ -1909,8 +1911,6 @@ static void K_HandleLapIncrement(player_t *player)
 
 			if (netgame && player->laps >= numlaps)
 				CON_LogMessage(va(M_GetText("%s has finished the race.\n"), player_names[player-players]));
-
-			player->starpostnum = 0;
 
 			if (P_IsDisplayPlayer(player))
 			{