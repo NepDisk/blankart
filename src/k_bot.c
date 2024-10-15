KartKrewDev/RingRacers

Add bot styles & bot spawn ACS function

src/k_bot.c
@@ -37,11 +37,11 @@
 #endif
 
 /*--------------------------------------------------
-	boolean K_AddBot(UINT8 skin, UINT8 difficulty, UINT8 *p)
+	boolean K_AddBot(UINT8 skin, UINT8 difficulty, botStyle_e style, UINT8 *p)
 
 		See header file for description.
 --------------------------------------------------*/
-boolean K_AddBot(UINT8 skin, UINT8 difficulty, UINT8 *p)
+boolean K_AddBot(UINT8 skin, UINT8 difficulty, botStyle_e style, UINT8 *p)
 {
 	UINT8 buf[3];
 	UINT8 *buf_p = buf;
@@ -96,6 +96,7 @@ boolean K_AddBot(UINT8 skin, UINT8 difficulty, UINT8 *p)
 	}
 
 	WRITEUINT8(buf_p, difficulty);
+	WRITEUINT8(buf_p, style);
 
 	SendNetXCmd(XD_ADDBOT, buf, buf_p - buf);
 
@@ -156,6 +157,9 @@ void K_UpdateMatchRaceBots(void)
 
 					// While we're here, we should update bot difficulty to the proper value.
 					players[i].botvars.difficulty = difficulty;
+
+					// Enforce normal style for Match Race
+					players[i].botvars.style = BOT_STYLE_NORMAL;
 				}
 				else
 				{
@@ -169,7 +173,7 @@ void K_UpdateMatchRaceBots(void)
 		}
 	}
 
-	if (difficulty == 0 || !(gametyperules & GTR_BOTS))
+	if (difficulty == 0 || (gametyperules & GTR_BOTS) == 0)
 	{
 		wantedbots = 0;
 	}
@@ -217,7 +221,7 @@ void K_UpdateMatchRaceBots(void)
 				grabskins[index] = grabskins[--usableskins];
 			}
 
-			if (!K_AddBot(skinnum, difficulty, &newplayernum))
+			if (!K_AddBot(skinnum, difficulty, BOT_STYLE_NORMAL, &newplayernum))
 			{
 				// Not enough player slots to add the bot, break the loop.
 				break;
@@ -231,7 +235,6 @@ void K_UpdateMatchRaceBots(void)
 		UINT8 buf[2];
 
 		i = MAXPLAYERS;
-
 		while (numbots > wantedbots && i > 0)
 		{
 			i--;
@@ -1473,11 +1476,11 @@ static void K_BuildBotPodiumTiccmd(player_t *player, ticcmd_t *cmd)
 }
 
 /*--------------------------------------------------
-	void K_BuildBotTiccmd(player_t *player, ticcmd_t *cmd)
+	static void K_BuildBotTiccmdNormal(player_t *player, ticcmd_t *cmd)
 
-		See header file for description.
+		Build ticcmd for bots with a style of BOT_STYLE_NORMAL
 --------------------------------------------------*/
-void K_BuildBotTiccmd(player_t *player, ticcmd_t *cmd)
+static void K_BuildBotTiccmdNormal(player_t *player, ticcmd_t *cmd)
 {
 	precise_t t = 0;
 	botprediction_t *predict = NULL;
@@ -1486,29 +1489,6 @@ void K_BuildBotTiccmd(player_t *player, ticcmd_t *cmd)
 	UINT8 spindash = 0;
 	INT32 turnamt = 0;
 	const line_t *botController = player->botvars.controller != UINT16_MAX ? &lines[player->botvars.controller] : NULL;
-
-	// Remove any existing controls
-	memset(cmd, 0, sizeof(ticcmd_t));
-
-	if (player->mo == NULL
-		|| player->spectator == true
-		|| G_GamestateUsesLevel() == false)
-	{
-		// Not in the level.
-		return;
-	}
-
-	// Complete override of all ticcmd functionality
-	if (LUA_HookTiccmd(player, cmd, HOOK(BotTiccmd)) == true)
-	{
-		return;
-	}
-
-	if (K_PodiumSequence() == true)
-	{
-		K_BuildBotPodiumTiccmd(player, cmd);
-		return;
-	}
 
 	if (!(gametyperules & GTR_BOTS) // No bot behaviors
 		|| K_GetNumWaypoints() == 0 // No waypoints
@@ -1759,6 +1739,59 @@ void K_BuildBotTiccmd(player_t *player, ticcmd_t *cmd)
 		}
 
 		Z_Free(predict);
+	}
+}
+
+/*--------------------------------------------------
+	void K_BuildBotTiccmd(player_t *player, ticcmd_t *cmd)
+
+		See header file for description.
+--------------------------------------------------*/
+void K_BuildBotTiccmd(player_t *player, ticcmd_t *cmd)
+{
+	precise_t t = 0;
+	botprediction_t *predict = NULL;
+	boolean trySpindash = true;
+	angle_t destangle = 0;
+	UINT8 spindash = 0;
+	INT32 turnamt = 0;
+	const line_t *botController = player->botvars.controller != UINT16_MAX ? &lines[player->botvars.controller] : NULL;
+
+	// Remove any existing controls
+	memset(cmd, 0, sizeof(ticcmd_t));
+
+	if (player->mo == NULL
+		|| player->spectator == true
+		|| G_GamestateUsesLevel() == false)
+	{
+		// Not in the level.
+		return;
+	}
+
+	// Complete override of all ticcmd functionality
+	if (LUA_HookTiccmd(player, cmd, HOOK(BotTiccmd)) == true)
+	{
+		return;
+	}
+
+	if (K_PodiumSequence() == true)
+	{
+		K_BuildBotPodiumTiccmd(player, cmd);
+		return;
+	}
+
+	switch (player->botvars.style)
+	{
+		case BOT_STYLE_STAY:
+		{
+			// Hey, this one's pretty easy :P
+			break;
+		}
+		default:
+		{
+			K_BuildBotTiccmdNormal(player, cmd);
+			break;
+		}
 	}
 }
 

NepDisk/blankart

src/k_bot.c
@@ -32,11 +32,11 @@
 #include "m_easing.h"
 
 /*--------------------------------------------------
-	boolean K_AddBot(UINT8 skin, UINT8 difficulty, UINT8 *p)
+	boolean K_AddBot(UINT8 skin, UINT8 difficulty, botStyle_e style, UINT8 *p)
 
 		See header file for description.
 --------------------------------------------------*/
-boolean K_AddBot(UINT8 skin, UINT8 difficulty, UINT8 *p)
+boolean K_AddBot(UINT8 skin, UINT8 difficulty, botStyle_e style, UINT8 *p)
 {
 	UINT8 buf[3];
 	UINT8 *buf_p = buf;
@@ -91,6 +91,7 @@ boolean K_AddBot(UINT8 skin, UINT8 difficulty, UINT8 *p)
 	}
 
 	WRITEUINT8(buf_p, difficulty);
+	WRITEUINT8(buf_p, style);
 
 	SendNetXCmd(XD_ADDBOT, buf, buf_p - buf);
 
@@ -155,6 +156,9 @@ void K_UpdateMatchRaceBots(void)
 
 					// While we're here, we should update bot difficulty to the proper value.
 					players[i].botvars.difficulty = difficulty;
+
+					// Enforce normal style for Match Race
+					players[i].botvars.style = BOT_STYLE_NORMAL;
 				}
 				else
 				{
@@ -226,7 +230,7 @@ void K_UpdateMatchRaceBots(void)
 				}
 			}
 
-			if (!K_AddBot(skin, difficulty, &newplayernum))
+			if (!K_AddBot(skin, difficulty, BOT_STYLE_NORMAL, &newplayernum))
 			{
 				// Not enough player slots to add the bot, break the loop.
 				break;
@@ -241,7 +245,6 @@ void K_UpdateMatchRaceBots(void)
 		UINT8 buf[2];
 
 		i = MAXPLAYERS;
-
 		while (numbots > wantedbots && i > 0)
 		{
 			i--;
@@ -1132,32 +1135,17 @@ static INT32 K_HandleBotReverse(player_t *player, ticcmd_t *cmd, botprediction_t
 }
 
 /*--------------------------------------------------
-	void K_BuildBotTiccmd(player_t *player, ticcmd_t *cmd)
+	static void K_BuildBotTiccmdNormal(player_t *player, ticcmd_t *cmd)
 
-		See header file for description.
+		Build ticcmd for bots with a style of BOT_STYLE_NORMAL
 --------------------------------------------------*/
-void K_BuildBotTiccmd(player_t *player, ticcmd_t *cmd)
+static void K_BuildBotTiccmdNormal(player_t *player, ticcmd_t *cmd)
 {
 	precise_t t = 0;
 	botprediction_t *predict = NULL;
 	angle_t destangle = 0;
 	INT32 turnamt = 0;
 	const line_t *botController = player->botvars.controller != UINT16_MAX ? &lines[player->botvars.controller] : NULL;
-
-	// Remove any existing controls
-	memset(cmd, 0, sizeof(ticcmd_t));
-
-	if (gamestate != GS_LEVEL || !player->mo || player->spectator)
-	{
-		// Not in the level.
-		return;
-	}
-
-	// Complete override of all ticcmd functionality
-	if (LUA_HookTiccmd(player, cmd, HOOK(BotTiccmd)) == true)
-	{
-		return;
-	}
 
 	if (!(gametyperules & GTR_BOTS) // No bot behaviors
 		|| K_GetNumWaypoints() == 0 // No waypoints
@@ -1297,6 +1285,52 @@ void K_BuildBotTiccmd(player_t *player, ticcmd_t *cmd)
 		}
 
 		Z_Free(predict);
+	}
+}
+
+/*--------------------------------------------------
+	void K_BuildBotTiccmd(player_t *player, ticcmd_t *cmd)
+
+		See header file for description.
+--------------------------------------------------*/
+void K_BuildBotTiccmd(player_t *player, ticcmd_t *cmd)
+{
+	precise_t t = 0;
+	botprediction_t *predict = NULL;
+	boolean trySpindash = true;
+	angle_t destangle = 0;
+	UINT8 spindash = 0;
+	INT32 turnamt = 0;
+	const line_t *botController = player->botvars.controller != UINT16_MAX ? &lines[player->botvars.controller] : NULL;
+
+	// Remove any existing controls
+	memset(cmd, 0, sizeof(ticcmd_t));
+
+	if (player->mo == NULL
+		|| player->spectator == true)
+	{
+		// Not in the level.
+		return;
+	}
+
+	// Complete override of all ticcmd functionality
+	if (LUA_HookTiccmd(player, cmd, HOOK(BotTiccmd)) == true)
+	{
+		return;
+	}
+
+	switch (player->botvars.style)
+	{
+		case BOT_STYLE_STAY:
+		{
+			// Hey, this one's pretty easy :P
+			break;
+		}
+		default:
+		{
+			K_BuildBotTiccmdNormal(player, cmd);
+			break;
+		}
 	}
 }
 